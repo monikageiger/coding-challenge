ce5d76412a26d7a833182ca505e77a00bb66769a
Monika Geiger <geigermonika@hotmail.hu>
2024-04-17 13:24:31 +0200

    added call to user profile

---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/moni/vue/coding-challenge/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
--- pages/user/[id].vue
+++ pages/user/[id].vue
@@ -1,11 +1,20 @@
 <template>
-  <article>
-    <h1>User profile</h1>
-  </article>
+    <article>
+        <h1>User profile</h1>
+        <p>Name: {{ data.name }}</p>
+        <p>Email: {{ data.email }}</p>
+        <p>Website: {{ data.website }}</p>
+        <p>Phone number: {{ data.phone }}</p>
+        <p v-if="data && data.company">Company name: {{ data.company.name }}</p>
+    </article>
 </template>
 
 <script setup>
-// Fetch user data
+const { id } = useRoute().params
+
+const { data, error } = await useFetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+)
 </script>
 
 <style></style>
